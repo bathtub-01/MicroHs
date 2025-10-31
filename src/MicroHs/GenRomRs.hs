c69809c983068852ca39feed26c9cfb57bb519a2
bathtub <yorkgank@gmail.com>
2025-10-31 10:47:14 +0000

    add: new printer

--- src/MicroHs/GenRomRs.hs
+++ src/MicroHs/GenRomRs.hs
@@ -1,14 +1,7 @@
 module MicroHs.GenRomRs where
 import Prelude(); import MHSPrelude
 import Data.List
-import qualified MicroHs.IdentMap as M
-import Data.Maybe
-import MicroHs.Desugar(LDef)
-import MicroHs.Exp
-import MicroHs.Expr(Lit(..), showLit, errorMessage, HasLoc(..))
-import MicroHs.Ident(Ident(..), showIdent, mkIdent)
 import MicroHs.State
-import MicroHs.GenRom(getPatNum,inlineSingle,finalEtaApply,freeText)
 import MicroHs.CodeGen
 
 -- generate ROM file for the Rust simulator
@@ -29,11 +22,11 @@ lazyLockProg name r =
 indentation :: Int -> String -> (String -> String)
 indentation level s = ((replicate (level * 4) ' ' ++ s) ++)
 
-vecS :: (String -> String) -> (String -> String)
-vecS r =
+vecS :: String -> (String -> String) -> (String -> String)
+vecS n r =
   let
     indent = indentation 1
-  in indent "vec![\n" . r . indent "]"
+  in indent (n ++ ": vec![\n") . r . indent "],\n"
 
 -- application
 app :: Int -> (String -> String) -> (String -> String)
@@ -50,7 +43,7 @@ atomIndent = indentation 3
 -- atoms
 comb :: Int -> Int -> (String -> String)
 comb art p =
-  atomIndent ("COM(" ++ show art ++ "," ++ show p ++ "," ++ "),\n")
+  atomIndent ("COM(" ++ show art ++ "," ++ show p ++ "),\n")
 
 opConvert :: String -> (String, String)
 opConvert "==" = ("EQ", "false")
@@ -89,22 +82,16 @@ err :: Int -> (String -> String)
 err n =
   atomIndent ("ERR(" ++ show n ++ "),\n")
 
-listPrint :: [Int] -> String
-listPrint [] = "[]"
-listPrint xs = "[" ++ inner ++ "]"
-  where
-    inner = concat $ zipWith (\x y -> show x ++ y) xs (replicate (length xs - 1) "," ++ [""])
-
 genRomRs :: String -> ([AExp], [AExp]) -> String
 genRomRs progName (heap, cmb) =
   let          
     (heap', cmb') = serialise heap cmb
-    heapStr = putAExpList heap'
-    cmbStr = putAExpList cmb'
+    heapStr = putAExpList "heap_img" heap'
+    cmbStr = putAExpList "comb_img" cmb'
   in header
      -- ++ "// Functions in this file: " ++ show funCount ++ "\n"
      -- ++ "// Apps in this file: " ++ show appCount ++ "\n"
-     -- ++ "// Combinators in this file: " ++ show combCount ++ "\n"
+     ++ "// Combinators in this file: " ++ show (length cmb) ++ "\n"
      ++ lazyLockProg progName (heapStr . cmbStr) ""
 
 serialise :: [AExp] -> [AExp] -> ([AExp], [AExp])
@@ -115,7 +102,7 @@ serialise heap cmb =
     walk _ (Com a p) = Com a $ ctbl !! p
     walk _ (Fun p) = Fun $ htbl !! p
     walk n (Ptr p b) | b = Ptr p b
-                     | otherwise = Ptr (p + htbl !! n) b
+                     | otherwise = Ptr (p + htbl !! n + 1) b
     walk _ a = a
     mapWalk :: [AExp] -> [AExp]
     mapWalk aes = map (\(ae, i) -> map (map (walk i)) ae) (zip aes [0..])
@@ -129,49 +116,40 @@ entryTable ass =
     walk (x:xs) ctr = ctr : walk xs (ctr + length x)
   in walk ass 0
 
-putAExpList :: [AExp] -> (String -> String)
-putAExpList aes = let
-  ((), (_, _, _, r)) = runState (mapM_ putAExp aes) (0, 0, 0, id)
-  in vecS r
+putAExpList :: String -> [AExp] -> (String -> String)
+putAExpList n aes = let
+  ((), (_, _, r)) = runState (mapM_ putAExp aes) (0, 0, id)
+  in vecS n r
 
-
-putAExp :: AExp -> State (Int, Int, Int, String -> String) ()
+-- state: 1. aexp counter; 2. app counter; 3. result string
+putAExp :: AExp -> State (Int, Int, String -> String) ()
 putAExp ae = do
+  (c1, c2, r) <- get
+  let comment = indentation 2 $ "// AExp" ++ show c1 ++ "\n"
+  put (c1 + 1, c2, r . comment)
   mapM_ putApp ae
-  (c1, c2, c3, r) <- get
-  put (c1 + 1, c2, c3, r)
 
-putApp :: App -> State (Int, Int, Int, String -> String) ()
+putApp :: App -> State (Int, Int, String -> String) ()
 putApp ap = do
-  (c1, c2, c3, r) <- get
-  put (c1, c2, c3, id)
+  (c1, c2, r) <- get
+  put (c1, c2, id)
   mapM_ putAtom ap
-  (c1', c2', c3', r') <- get
-  put (c1', c2' + 1, c3', r . vecS r')
+  (c1', c2', r') <- get
+  put (c1', c2' + 1, r . app c2' r')
 
-putAtom :: Atom -> State (Int, Int, Int, String -> String) ()
+putAtom :: Atom -> State (Int, Int, String -> String) ()
 putAtom atm =
   let
-    atom (Prm s) = y
+    atom (Prm "Y") = y
+    atom (Prm "seq") = seqStr
+    atom (Prm op) = if "error" `isPrefixOf` op
+                      then err $ read (drop 5 op)
+                      else prim op
     atom (Int i) = int i
     atom (Com a p) = comb a p
     atom (Fun p) = ptr p False
     atom (Ptr p hc) = ptr p hc
     atom (Apt p) = arg p
   in do
-  (c1, c2, c3, r) <- get  
-  put (c1, c2, c3 + 1, r . atom atm)
-
--- atom :: Exp -> (String -> String)
--- atom ae =
---   case ae of
---     Var i -> if "PTR" `isPrefixOf` showIdent i then ptr $ read (drop 3 (showIdent i))
---                else error "Strange Var exists."
---     Lit (LInt i) -> int i
---     Lit (LPrim "Y") -> y
---     Lit (LPrim "seq") -> seqStr
---     Lit (LPrim op) -> if "error" `isPrefixOf` op then err $ read (drop 5 op)
---                         else prim op
---     Lit _ -> error "Strange Lit exists."
---     Sc a p is -> comb a p is
---     _ -> error "Not an Atom."
+  (c1, c2, r) <- get  
+  put (c1, c2, r . atom atm)
